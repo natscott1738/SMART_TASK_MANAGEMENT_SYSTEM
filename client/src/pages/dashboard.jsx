9b8363910554a036aef8e411cc52291f4271151e
natscott1738 <natscott383@gmail.com>
2025-01-02 14:35:11 +0300

    table data

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import {
   MdAdminPanelSettings,
   MdKeyboardArrowDown,
@@ -12,6 +12,126 @@ import moment from "moment";
 import clsx from 'clsx';
 import Chart from "../components/Chart";
 import { summary } from '../assets/data';
+import { TASK_TYPE, PRIORITYSTYLES, BGS, getInitials } from "../utils";
+import UserInfo from '../components/UserInfo';
+
+const TaskTable = ({ tasks }) => {
+  const ICONS = {
+    high: <MdKeyboardDoubleArrowUp />,
+    medium: <MdKeyboardArrowUp />,
+    low: <MdKeyboardArrowDown />,
+};
+
+const TableHeader = ()=> (
+  <thead className='border-b border-gray-300'>
+    <tr className='text-black text-left'>
+      <th className='py-2'>Task Title</th>
+      <th className='py-2'>Priority</th>
+      <th className='py-2'>Team</th>
+      <th className='py-2 hidden md:block'>Created At</th>
+    </tr>
+  </thead>
+);
+
+const TableRow = ({task}) => (
+  <tr className='border-b border-gray-300 text-gray-600 hover:bg-gray-300/10'>
+    <td className='py-2'>
+
+      <div className='flex items-center gap-2'>
+        <div
+        className={clsx('w-4 h-4 rounded-full', TASK_TYPE[task.stage])}
+        />
+
+        <p className='text-base text-black'>{task.title}</p>
+      </div>
+    </td>
+    <td className='py-2'>
+      <div className='flex gap-1 items-center'>
+        <span className={clsx('text-lg', PRIORITYSTYLES[task.priority])}>
+          {ICONS[task.priority]}</span>
+        <span className='capitalize'>{task.priority}
+        </span>
+      </div>
+    </td>
+
+    <td className='py-2'>
+      <div className='flex'>
+    {task.team.map((m, index)=> (
+      <div
+      key={index}
+
+      className={clsx('w-7 h-7 rounded-full text-white flex items-center justify-center text-sm -mr-1', BGS[index % BGS.length])}>
+      
+      <UserInfo user={m}/>
+      </div>
+    ))}
+      </div>
+
+    </td>
+
+    <td className='py-2 hidden md:block'>
+      <span className='text-base text-gray-600'>{moment(task?.date).fromNow()}</span>
+    </td>
+  </tr>
+);
+return (
+  <>
+  <div className='w-full md:w-2/3 bg-white px-2 md:px-4 pt-4 pb-4 shadow-md rounded'>
+  <table className='w-full'>
+    <TableHeader />
+    <tbody>
+      {tasks?.map((task, id)=> (
+        <TableRow key={id} task={task} />
+      ))}
+    </tbody>    
+  </table>
+  </div>
+  </>
+);
+};
+
+const UserTable = ({users}) => {
+  const TableHeader = () => (
+    <thead className='border-b border-gray-300 '>
+      <tr className='text-black  text-left'>
+        <th className='py-2'>Full Name</th>
+        <th className='py-2'>Status</th>
+        <th className='py-2'>Created At</th>
+      </tr>
+    </thead>
+  );
+
+  const TableRow =({user})=> (
+    <tr className='border-b border-gray-300 text-gray-600 hover:bg-gray-300/10'>
+    <td className='p-2'>
+    <div className='flex items-center gap-3'>
+
+      <div className='w-9 h-9 rounded-full text-white flex items-center justify-center text-sm bg-violet-700'>
+        <span className='text-center'>{getInitials(user?.name)}</span>
+      </div>
+      <div>
+        <p> {user.name} </p>
+        <span className='text-xs text-black'>{user?.role}</span>
+      </div>
+    </div>
+    </td>
+    </tr>
+
+  );
+
+  return (
+    <div className='w-full md:w-1/3 bg-white h-fit px-2 md:px-6 py-4 shadow-md rounded'>
+      <table className='w-full mb-5'>
+        <TableHeader />
+        <tbody>
+          {users?.map((user, index) => (
+            <TableRow key={index + user?._id} user={user} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  ); 
+};
 
 const Dashboard = () => {
   const totals = summary.tasks;
@@ -87,7 +207,16 @@ const Dashboard = () => {
         <Chart />
       </div>
 
-      <div className='w-full flex flex-col md:flex-row gap-4'>
+      <div className='w-full flex flex-col md:flex-row gap-4 2xl:gap-10 py-8'>
+      {/* left */}
+      
+        <TaskTable 
+        tasks={summary.last10Task}
+        />
+
+      {/* right */}
+
+      <UserTable users={summary.users} />
 
       </div>
     </div>
