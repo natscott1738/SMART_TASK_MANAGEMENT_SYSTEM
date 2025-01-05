c404fe7c06584b6a911a10a2ba33d8c289cf731c
natscott1738 <natscott383@gmail.com>
2025-01-05 10:36:34 +0300

    task details page[tasks]

--- client/src/pages/TaskDetails.jsx
+++ client/src/pages/TaskDetails.jsx
@@ -16,7 +16,7 @@ import { useParams } from "react-router-dom";
 import { toast } from "sonner";
 import { tasks } from '../assets/data';
 import Tabs from "../components/Tabs";
-import { PRIORITYSTYLES, TASK_TYPE } from '../utils';
+import { PRIORITYSTYLES, TASK_TYPE, getInitials } from '../utils';
 
 
 const assets = [
@@ -125,8 +125,102 @@ const TaskDetails = () => {
                   </div>                  
 
           </div>
+
+          <p className='text-gray-500'>
+            Created At: {new Date(task?.date).toDateString()}
+          </p>
+
+          <div className='flex items-center gap-8 p-4 border-y border-gray-200'>
+              <div className='space-x-2'>
+                <span className='font-semibold'>Assets :</span>
+                <span>{task?.assets?.length}</span>
+              </div>
+
+              <span className='text-gray-400'>|</span>
+
+              <div className='space-x-2'>
+                <span className='font-semibold'>Sub-Task :</span>
+                <span>{task?.subTasks?.length}</span>
+              </div>
+            </div>
+
+            <div className="space-y-4 py-6">
+              <p className="text-gray-600 font-semibold text-sm">TASK TEAM</p>
+
+              <div className="space-y-3">
+                  {
+                    task?.team?.map((m, index) => (
+                      <div
+                      key={index}
+                      className="flex gap-4 py-2 items-center border-t border-gray-200"
+                      >
+                        <div
+                          className={
+                            "w-10 h-10 rounded-full text-white flex items-center justify-center text-sm -mr-1 bg-blue-600"
+                          }
+                        >
+                          <span className='text-center'>
+                            {getInitials(m?.name)}
+                          </span>
+                        </div>
+
+                        <div>
+                          <p className="text-lg font-semibold">{m?.name}</p>
+                          <span className="text-gray-500">{m?.title}</span>
+                        </div>
+                      </div>
+                    ))}
+              </div>
+            </div>
+
+            <div className="space-y-4 py-6">
+            <p className='text-gray-500 font-semibold text-sm'>
+              SUB-TASKS
+            </p>
+            <div className="space-y-8">
+              {
+                task?.subTasks?.map((el, index) => (
+                  <div
+                  key={index}
+                  className="flex gap-3"
+                  >
+                    <div className="w-10 h-10 flex items-center justify-center rounded-full bg-violet-200">
+                    <MdTaskAlt className='text-violet-600' size={26} />
+                    </div>
+
+                    <div className="space-y-1">
+                    <div className="flex gap-2 items-center">
+                      <span className="text-sm text-gray-500">{new Date(el?.date).toDateString()}</span>
+                      <span className="px-2 py-0.5 text-center text-sm rounded-full bg-violet-100 text-violet-700 font-semib
+                      ">{el?.tag}</span>
+                    </div>
+
+                    <p className="text-gray-700">{el?.title}</p>
+                    </div>
+                  </div>
+                ))
+              }
+            </div>
+            </div>
           </div>
-          <div className="w-full md:w-1/2 space-y-8"></div>
+
+          {/* RIGHT */}
+          <div className="w-full md:w-1/2 space-y-8">
+          <p className='text-lg font-semibold'>ASSETS</p>
+
+          <div className="w-full grid grid-cols-2 gap-4">
+              {
+                task?.assets?.map((el, index) => (
+                  <img
+                  key={index}
+                  src={el}
+                  alt={task?.title}
+                  className="w-full rounded h-28 md:h-36 2xl:h-52 cursor:pointer transition-all duration-700 hover:scale-125 hover:z-50"
+                  />
+                ))
+              }
+          </div>
+          </div>
           </div>
           </> : <></>
         }
